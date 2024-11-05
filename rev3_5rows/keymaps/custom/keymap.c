50baafb2c1e9d49a489d37746bfd5852f3f9842c
Claudio Lepín Mena <azratul@gmail.com>
2024-11-05 11:42:20 -0300

    New layout

--- rev3_5rows/keymaps/custom/keymap.c
+++ rev3_5rows/keymaps/custom/keymap.c
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RAISE,
-      KC_LSFT, KC_NUBS, KC_LBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_PSCR, KC_QUOT, KC_RBRC, KC_SPC,  KC_SPC,  KC_RALT, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      RAISE,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_LBRC, KC_RBRC, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_PSCR, KC_QUOT, KC_MINS, KC_SPC,  KC_SPC,  KC_RALT, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
       KC_CALC, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL,
-      KC_MINS, _______, _______, _______, _______, KC_LPAD,                   KC_MENU, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, KC_LPAD,                   KC_MENU, _______, _______, _______, _______, ADJUST,
       KC_CAPS, KC_HOME, KC_PCMM, KC_PDOT, KC_WHOM, KC_CPNL, KC_EQL,  KC_UNDS, KC_MUTE, KC_MPRV, KC_MFFD, KC_MSTP, KC_END,  KC_INS,
       KC_APP,  KC_WBAK, KC_WFWD, KC_WREF, KC_PLUS, CARET,   KC_PGDN, KC_PGUP, GRAVE,   KC_EXLM, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
       ),
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] =  LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
       _______, _______, _______, _______, _______, AG_NORM,                   KC_RSFT, UG_TOGG, UG_HUEU, UG_SATU, UG_VALU, KC_DEL,
-      _______, _______, _______, _______, _______, _______,                   _______, UG_NEXT, UG_HUED, UG_SATD, UG_VALD, ADJUST,
+      ADJUST,  _______, _______, _______, _______, _______,                   _______, UG_NEXT, UG_HUED, UG_SATD, UG_VALD, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
       )
