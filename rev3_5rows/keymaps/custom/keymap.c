686108f05d8b40e062ad0940730e884437335ba9
Claudio Lepín Mena <azratul@gmail.com>
2024-12-13 10:41:51 -0300

    new layout

--- rev3_5rows/keymaps/custom/keymap.c
+++ rev3_5rows/keymaps/custom/keymap.c
@@ -31,7 +31,8 @@ enum custom_keycodes {
   ADJUST,
   RGBRST,
   CARET,
-  GRAVE
+  BCKTICK,
+  DEGREE
 };
 
 #define LOWER MO(_LOWER)
@@ -52,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-      RAISE,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_LBRC, KC_RBRC, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_PSCR, KC_QUOT, KC_MINS, KC_SPC,  KC_SPC,  KC_RALT, KC_BSLS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    CARET,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+      RAISE,   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+      KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,   KC_V,    BCKTICK, KC_RBRC, KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_PSCR, KC_EQL, KC_MINS, KC_LBRC, KC_SPC,  KC_RALT, KC_UNDS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
@@ -96,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
       KC_CALC, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL,
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, ADJUST,
-      KC_CAPS, KC_HOME, KC_PCMM, _______, _______, _______, KC_EQL,  KC_UNDS, _______, _______, KC_MPRV, KC_MFFD, KC_END,  KC_MSTP,
-      KC_APP,  KC_WBAK, KC_WFWD, KC_WREF, KC_INS,  KC_PLUS, KC_PGDN, KC_PGUP, KC_EXLM, KC_MUTE, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+      KC_CAPS, KC_HOME, KC_PCMM, _______, _______, _______, KC_INS,  KC_MUTE, _______, _______, KC_MPRV, KC_MFFD, KC_END,  KC_MSTP,
+      KC_APP,  KC_WBAK, KC_WFWD, KC_WREF, KC_PLUS, DEGREE,  KC_PGDN, KC_PGUP, _______, KC_EXLM, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
       ),
 
   /* Adjust (Lower + Raise)
@@ -182,6 +183,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case CARET:
       if (record->event.pressed) {
         tap_code16(RALT(KC_QUOT));
+      }
+      return false;
+      break;
+    case BCKTICK:
+      if (record->event.pressed) {
+        tap_code16(RALT(KC_BSLS));
+      }
+      return false;
+      break;
+    case DEGREE:
+      if (record->event.pressed) {
+        tap_code16(LSFT(KC_GRV));
       }
       return false;
       break;
